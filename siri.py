731523f6a080f3d6c3f30e19a8b4366984622cfb
Siri1324 <140593752+Siri1324@users.noreply.github.com>
2024-01-12 10:35:25 +0530

    Update siri.py

--- siri.py
+++ siri.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header("puppy")
 st.write("puppy")
-st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fpuppy&psig=AOvVaw3hRjKwaGovMPUIY26FQzzr&ust=1705121173770000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCJCBs8CF14MDFQAAAAAdAAAAABAD",caption="puppy")
+st.image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fpuppy&psig=AOvVaw3hRjKwaGovMPUIY26FQzzr&ust=1705121173770000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCJCBs8CF14MDFQAAAAAdAAAAABAD',caption="puppy")
