34cbc528e690bdeb16b8007da8e8816cb6998636
Siri1324 <140593752+Siri1324@users.noreply.github.com>
2024-01-12 10:42:52 +0530

    Update siri.py

--- siri.py
+++ siri.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header("puppy")
 st.write("puppy")
-st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fdogtime.com%2Fpuppies%2F255-puppies&psig=AOvVaw3hRjKwaGovMPUIY26FQzzr&ust=1705121173770000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCJCBs8CF14MDFQAAAAAdAAAAABAI",caption="puppy")
+st.image("https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1601979031925-424e53b6caaa%3Fq%3D80%26w%3D1000%26auto%3Dformat%26fit%3Dcrop%26ixlib%3Drb-4.0.3%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8M3x8cHVwcHl8ZW58MHx8MHx8fDA%253D&tbnid=qlkqBCEI5YpF3M&vet=12ahUKEwjohKaji9eDAxXLZGwGHaSNA70QMygAegQIARBs..i&imgrefurl=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fpuppy&docid=2ssGwWYAAf269M&w=1000&h=1500&q=puppy&ved=2ahUKEwjohKaji9eDAxXLZGwGHaSNA70QMygAegQIARBs",caption="puppy")
